02dd8fdb5a7a66c9db04023bd5d2ff22b2b489bf
lucky <fanqietou@126.com>
2019-01-02 23:19:26 +0800

    2019/01/02 commit by lucky

---
+++ java/com/web/business/login/model/User.java
@@ -0,0 +1,96 @@
+package com.web.business.login.model;
+
+import java.sql.Timestamp;
+
+public class User {
+
+	private int id;
+
+	private String name;
+
+	private String pwd;
+	
+	private String tel;
+	
+	private String email;
+	
+	private int roleId;
+
+	private Timestamp createTime;
+
+	private Timestamp updateTime;
+	
+	private char isDelete;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+
+	public Timestamp getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Timestamp updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public char getIsDelete() {
+		return isDelete;
+	}
+
+	public void setIsDelete(char isDelete) {
+		this.isDelete = isDelete;
+	}
+}
